9143800b5d76f0bbd86349733d4e1ca4e7ed69c7
Jenelle Feather <jenellefeather@gmail.com>
2023-12-13 17:07:19 -0500

    making jsin warning more descriptive

--- default_paths.py
+++ default_paths.py
@@ -9,4 +9,4 @@ MODEL_CHECKPOINT_DIR = os.path.join(WORKING_DIRECTORY, 'model_checkpoints')
 JSIN_PATH = '/om4/group/mcdermott/projects/ibmHearingAid/assets/data/datasets/JSIN_v3.00/nStim_20000/2000ms/rms_0.1/noiseSNR_-10_10/stimSR_20000/reverb_none/noise_all/JSIN_all_v3/subsets'
 if not os.path.exists(JSIN_PATH):
     JSIN_PATH = None
-    print('### WARNING: UNABLE TO FIND JSIN AUDIO TRAINING DATASET FILES. IF TRAINING AUDIO MODELS, CHANGE PATH SPECIFIED IN analysis_scripts/default_paths.py. METAMERS CAN BE GENERATED WITHOUT THESE FILES ###')
+    print('### WARNING: UNABLE TO FIND JSIN AUDIO TRAINING DATASET FILES. IF TRAINING AUDIO MODELS, CHANGE PATH SPECIFIED IN analysis_scripts/default_paths.py. MODELS CAN BE LOADED AND TESTED WITHOUT THESE FILES. ###')
